c7daf884f839cf62ea1970e9366ff15cb62d8a1f
Collins Daberechukwu <collinsdaberechi20@gmail.com>
2024-01-06 11:49:44 +0100

    first commit

---
+++ src/main/java/com/example/restemplateapi/ResTemplateApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.restemplateapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTemplateApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResTemplateApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/restemplateapi/ResTemplateApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restemplateapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResTemplateApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
